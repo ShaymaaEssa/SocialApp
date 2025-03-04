00beac14b256b45f95c0af29ba1a56b2b109d26a
EM <visualprincess1999@hotmail.com>
2025-03-04 13:31:22 +0200

    initalize

--- src/app/core/services/users/users.service.ts
+++ src/app/core/services/users/users.service.ts
@@ -1,9 +1,35 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environment/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
 
-  constructor() { }
+  constructor(private readonly httpClient :HttpClient) { }
+  
+  signup(data:object):Observable<any>{
+    return this.httpClient.post(`${environment.baseURL}users/signup`, data)
+  }
+
+  signin(data:object):Observable<any>{
+    return this.httpClient.post(`${environment.baseURL}users/signin`, data)
+  }
+
+  changePassword(data:object):Observable<any>{
+    return this.httpClient.patch(`${environment.baseURL}users/change-password`, data)
+  }
+
+  uploadProfilePhoto(data:object):Observable<any>{
+    return this.httpClient.patch(`${environment.baseURL}users/upload-photo`, data)
+
+  }
+
+  getLoggedUserData(data:object):Observable<any>{
+    return this.httpClient.patch(`${environment.baseURL}users/upload-photo`, data)
+
+  }
+
 }
